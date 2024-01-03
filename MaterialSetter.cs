dcdb52d6c88401769805d92ddaf0c138f266b246
Firoz Thapa <154414703+Firoz-Thapa@users.noreply.github.com>
2024-01-03 16:06:09 +0200

    Add files via upload

---
+++ MaterialSetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MaterialSetter : MonoBehaviour {
+
+
+    public Material Main;
+    public List<MeshRenderer> AllM = new List<MeshRenderer>();
+
+	void Update () { 
+        if (Main != null)
+        {
+            MeshRenderer[] renderers = GetComponentsInChildren<MeshRenderer>();
+            foreach (var r in renderers)
+            {
+      
+                r.material = Main;
+            }
+        }
+	}
+}
